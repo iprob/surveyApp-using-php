2253b0adc7374943e6964e28978fc55f4a30ea21
Tuhin Tarif <tuhintarif@Tuhins-MacBook-Pro.local>
2020-03-26 00:10:09 +0800

    Added creatingMcqRequest

--- app/Http/Requests/CreateMcqRequest.php
+++ app/Http/Requests/CreateMcqRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateMcq extends FormRequest
+class CreateMcqRequest extends FormRequest
 {
     public function authorize()
     {
