1011c3375011316a99c2f32284c0a5a448afea38
Tuhin Tarif <tuhintarif@Tuhins-MacBook-Pro.local>
2020-03-26 20:54:50 +0800

    Fixed route

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
+#survey Route
+
+Route::get('/question/{question}-{slug}', 'SurveyController@show');
+
 #Question Route
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/question/create','QuestionController@create');
 Route::post('/question','QuestionController@store');
-Route::get('/question/{question}','QuestionController@show');
+Route::get('/question/{question}','QuestionController@show')->where('id', '[0-9]+');
 Route::get('/question/{question}/edit','QuestionController@edit');
 
 #MCQ Rute
@@ -32,6 +37,5 @@ Route::get('/question/{question}/edit','QuestionController@edit');
 Route::get('/question/{question}/mcq/create','McqController@create');
 Route::post('/question/{question}/mcq','McqController@store');
 
-#survey Route
 
-Route::get('/question/{question}-{slug}', 'SurveyController@show');
+
